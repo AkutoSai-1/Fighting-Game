ad3b1b4d694f3bd3c0e571dc9981ff03db174c5a
unknown <ishmealmanueal@gmail.com>
2025-05-13 19:54:12 -0400

    second commit

--- JS/idk.js
+++ JS/idk.js
@@ -10,19 +10,26 @@ class Sprite {
    constructor({ position, velocity }) {
      this.position = position 
      this.velocity = velocity 
+     this.height = 150
    }
 
    draw() {
     c.fillStyle = 'Red'
-    c.fillRect(this.position.x, this.position.y, 50, 150)
+    c.fillRect(this.position.x, this.position.y, 50, this.height)
   }
   
     update() {
       this.draw()
-      this.position.y += 10 
-    }
-   
-} 
+      this.position.y += this.velocity.y
+
+      if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+        
+      } {
+        this.velocity.y = 0
+      }
+      }
+}
+ 
 const player = new Sprite ({
   position: {
     x: 0,
@@ -30,7 +37,7 @@ const player = new Sprite ({
   },
   velocity: {
     x: 0,
-    y: 0 
+    y: 10 
   }
 })
 
