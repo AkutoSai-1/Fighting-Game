b93845e9887d7e7cd0e5354a7614545bab91ae9a
unknown <ishmealmanueal@gmail.com>
2025-05-13 19:56:37 -0400

    second commit

--- JS/idk.js
+++ JS/idk.js
@@ -23,11 +23,9 @@ class Sprite {
       this.position.y += this.velocity.y
 
       if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-        
-      } {
         this.velocity.y = 0
       }
-      }
+    }
 }
  
 const player = new Sprite ({
@@ -59,8 +57,7 @@ function animate() {
   c.fillStyle = 'black'
   c.fillRect(0, 0, canvas.width, canvas.height)
   player.update()
-  enemy.update() 
-  console.log('Go')
+  enemy.update()
 }
 
 animate()
