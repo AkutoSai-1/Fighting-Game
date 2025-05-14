5e0d4533fc9b2c24cd8d59e84a89c5b6a85a5a1f
unknown <ishmealmanueal@gmail.com>
2025-05-14 00:52:16 -0400

    Resolved merge conflicts

---
+++ JS/main.js
@@ -0,0 +1,108 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext ('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const gravity = 0.2 
+class Sprite {
+   constructor({ position, velocity }) {
+     this.position = position 
+     this.velocity = velocity 
+     this.height = 150
+   }
+
+   draw() {
+    c.fillStyle = 'Red'
+    c.fillRect(this.position.x, this.position.y, 50, this.height)
+  }
+  
+    update() {
+      this.draw()
+      this.position.x += this.velocity.x
+      this.position.y += this.velocity.y
+
+      if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+        this.velocity.y = 0
+      } else this.velocity.y += gravity
+    }
+}
+ 
+const player = new Sprite ({
+  position: {
+    x: 256,
+    y: 100,
+  },
+  velocity: {
+    x: 0,
+    y: 10 
+  }
+})
+
+const enemy = new Sprite ({
+  position: {
+    x: 768,
+    y: 100,             
+  },
+  velocity: {
+    x: 0,
+    y: 10 
+  }
+})
+
+console.log(player)
+
+const keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  }
+}
+
+function animate() {
+  window.requestAnimationFrame(animate)
+  c.fillStyle = 'black'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  player.update()
+  enemy.update()
+
+  player.velocity.x = 0
+  
+  if (keys.a.pressed) {
+    player.velocity.x = -1.5
+  } else if (keys.d.pressed) {
+    player.velocity.x = 1.5
+  }
+}
+
+animate()
+
+window.addEventListener('keydown', (event) => {
+  switch (event.code) {
+    case 'KeyA':
+      keys.a.pressed = true
+      break
+    case 'KeyD':
+      keys.d.pressed = true
+      break
+  }
+  
+  console.log (event)
+})
+
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'a':
+      keys.a.pressed = false
+      break
+    case 'd':
+      keys.d.pressed = false
+      break
+  }
+  
+  console.log (event.key)
+})
