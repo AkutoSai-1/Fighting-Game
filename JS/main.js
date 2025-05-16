f6eb4f3937102f051c1c295000ee4952fc40f5fb
unknown <ishmealmanueal@gmail.com>
2025-05-15 21:48:28 -0400

    Commit

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,5 @@
+// Main
+
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext ('2d')
 
@@ -41,3 +43,137 @@ const Player2 = new Sprite ({
 })
 
 console.log(Player1)
+
+// Main End
+
+
+
+
+
+
+// Animate Start
+
+function animate() {
+  window.requestAnimationFrame(animate)
+  c.fillStyle = 'black'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  Player1.update()
+  Player2.update()
+  
+  let speed = 4.5
+  if (keys.shiftleft.pressed) speed *= 2
+  if (keys.shiftright.pressed) speed *= 2
+
+  Player1.velocity.x = 0
+  Player2.velocity.x = 0
+ 
+  // Player1
+
+  if (keys.a.pressed && Player1.lastKey === 'a') {
+    Player1.velocity.x = -speed
+  } else if (keys.d.pressed && Player1.lastKey === 'd') {
+    Player1.velocity.x = speed
+  }
+  
+  // Player2
+  
+  if (keys.ArrowLeft.pressed && Player2.lastKey === 'ArrowLeft') {
+    Player2.velocity.x = -speed
+  } else if (keys.ArrowRight.pressed && Player2.lastKey === 'ArrowRight') {
+    Player2.velocity.x = speed
+  }
+}
+
+animate()
+
+// Animate End
+
+
+
+
+
+
+// Movement Start
+
+const keys = {
+  a: {pressed: false},
+  d: {pressed: false},
+  ArrowLeft: {pressed: false},
+  ArrowRight: {pressed: false},
+  shiftleft: {pressed: false},
+  shiftright: {pressed: false}
+}
+
+let lastKey;
+
+window.addEventListener('keydown', (event) => {
+  switch (event.code) {
+    case 'KeyA':
+      keys.a.pressed = true
+      Player1.lastKey = 'a';
+      break
+    case 'KeyD':
+      keys.d.pressed = true
+      Player1.lastKey = 'd';
+      break
+    case 'KeyW':
+      Player1.velocity.y = -20
+      break
+    case 'ShiftLeft':
+      keys.shiftleft.pressed = true
+      break
+  }
+    
+  // Player2
+
+  switch (event.code) {  
+    case 'ArrowLeft':
+      keys.ArrowLeft.pressed = true 
+      Player2.lastKey = 'ArrowLeft';
+      break
+    case 'ArrowRight': 
+      keys.ArrowRight.pressed = true
+      Player2.lastKey = 'ArrowRight';
+      break
+    case 'ArrowUp':
+      Player2.velocity.y = -20
+      break
+    case 'ShiftRight':
+      keys.shiftright.pressed = true
+      break
+  }
+  
+  console.log (event.code)
+})
+
+window.addEventListener('keyup', (event) => {
+  switch (event.code) {
+    case 'KeyA':
+      keys.a.pressed = false
+      break
+    case 'KeyD':
+      keys.d.pressed = false
+      break
+    case 'ShiftLeft':
+      keys.shiftleft.pressed = false
+      break
+  }
+  
+  // Player2
+
+  switch (event.code) {  
+    case 'ArrowLeft':
+      keys.ArrowLeft.pressed = false
+      break
+    case 'ArrowRight':
+      keys.ArrowRight.pressed = false
+      break
+    case 'ShiftRight':
+      keys.shiftright.pressed = false
+      break
+  }
+
+  console.log (event.code)
+})
+
+// Movement End
